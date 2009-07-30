61809d4d0f4bb1e76d69fee593ff0a560db0577a
ru2def <ru2def@7cc6e8d0-7d37-11de-b4ed-11ca25030cd1>
2009-07-30 21:29:36 +0000

    marking as revision 3417 of elgg repository

---
+++ rev-3417/upgrade.php
@@ -0,0 +1,35 @@
+<?php
+	/**
+	 * Elgg upgrade script.
+	 * 
+	 * This script triggers any upgrades necessary, ensuring that upgrades are triggered deliberately by a single
+	 * user.
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008-2009
+	 * @link http://elgg.org/
+	 */
+
+	// Include elgg engine
+	define('upgrading','upgrading');
+	define('externalpage',true);
+	require_once(dirname(__FILE__) . "/engine/start.php");
+	
+	if (get_input('upgrade') == 'upgrade') {
+		if (version_upgrade_check()) {
+			version_upgrade();
+		}
+		datalist_set('simplecache_lastupdate',0);
+		
+		elgg_filepath_cache_reset();
+	} else {
+		global $CONFIG;
+		echo elgg_view('settings/upgrading');
+		exit;
+	}
+	
+	forward();
+?>
