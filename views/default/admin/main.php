cf402b0c18175f5ea4042953e7cd70c24eace14c
ru2def <ru2def@7cc6e8d0-7d37-11de-b4ed-11ca25030cd1>
2009-07-30 20:45:17 +0000

    imported elgg repository

---
+++ views/default/admin/main.php
@@ -0,0 +1,19 @@
+<?php
+
+	/**
+	 * Elgg administration main screen
+	 * 
+	 * @package Elgg
+	 * @subpackage Core
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd
+	 * @copyright Curverider Ltd 2008-2009
+	 * @link http://elgg.org/
+	 */
+
+	// Description of what's going on 
+		echo "<p>" . elgg_view('output/longtext', array('value' => elgg_echo("admin:description"))) . "</p>";
+		
+	
+		
+?>
